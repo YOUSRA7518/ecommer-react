fe3db70b8cb0bc2b55060d3b2696f42d1f8001e1
Yousra <chahi.yousraa@gmail.com>
2019-08-12 23:11:58 +0200

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+
+export default class ProductList extends Component {
+  state = {
+    products: []
+  };
+  render() {
+    console.log(this.state.products);
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+//<Product />;
